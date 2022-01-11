f8c0e13b06ff537d257d9cebe4a822087dcc23a1
Colestra <vitor.betti@hotmail.com>
2022-01-11 11:46:03 -0300

    JS alterado

--- Perguntas.js
+++ Perguntas.js
@@ -20,30 +20,51 @@ var pergunta4 = {
     respostas: ["3%", "47%", "11%"  ]
 }
 
+var acertos = 0;
+var erros = 0
+var fim = []
 var perguntas = [pergunta1, pergunta2, pergunta3, pergunta4]
 var perguntaSorteada;
+var numeroperguntaSorteada;
+var indice;
 
-
+ 
+  
 function sortearPergunta(){
+  var divResultado = document.getElementById("resultado");
+
     var perguntaHTML = document.getElementById("resposta");
     var mostraPergunta = document.getElementById("pergunta")
+    if(perguntas.length == 0  ){
+              htmlResultado = "<h1 class='resultado-final'>O JOGO ACABOU<h1><br> <h1> Você acertou :" + acertos+ "<br> <h1> Errou: "+erros;
 
-    var numeroperguntaSorteada = parseInt(Math.random()*4);
-    perguntaSorteada = perguntas[numeroperguntaSorteada]
+    }else {
+       
+        numeroperguntaSorteada = parseInt(Math.random()*perguntas.length);
+        perguntaSorteada = perguntas[numeroperguntaSorteada]
 
-    var pe = perguntaSorteada.pergunta;
-    var opcoesTexto = "";
-    mostraPergunta.innerHTML = pe;
+        var pe = perguntaSorteada.pergunta;
+        var opcoesTexto = "";
+        mostraPergunta.innerHTML = pe;
+            
         
-     
-        for (var resposta in perguntaSorteada.respostas){
+            for (var resposta in perguntaSorteada.respostas){
 
-                opcoesTexto += "<label class='container'> <input type='radio' name='resposta' value='"+ resposta + "'>  " +perguntaSorteada.respostas[resposta] + " <span class='checkmark'></span></label><br>";   
-        }
-        ;
-        perguntaHTML.innerHTML = opcoesTexto;
+                    opcoesTexto += "<label class='container'> <input type='radio' name='resposta' value='"+ resposta + "'>  " +perguntaSorteada.respostas[resposta] + " <span class='checkmark'></span></label><br>";   
+            }
+            ;
+            perguntaHTML.innerHTML = opcoesTexto;
+            document.getElementById("btnJogar").disabled = false;
+            document.getElementById("btnSortear").disabled = true;
 
+            console.log("Tamanho Array " + perguntas.length)
+            console.log("Numero Sorteadro " + perguntas.length)
+            }
+            
+            divResultado.innerHTML = htmlResultado;
+        
 }
+
 
 function obtemAtributoSelecionado(){
   
@@ -62,17 +83,32 @@ function jogar(){
   var divResultado = document.getElementById("resultado");
  
   
-  var valorRespostaSelecionada = perguntaSorteada.respostas[atributoSelecionado];
+  var valorRespostaSelecionada = perguntaSorteada.respostas[atributoSelecionado];   
+
   
-  if(valorRespostaSelecionada == perguntaSorteada.respostaCorreta){
-      htmlResultado = "<h1 class='resultado-final'>Você Acertou</h1>"; 
-      }else if(valorRespostaSelecionada < perguntaSorteada.respostaCorreta || valorRespostaSelecionada > perguntaSorteada.respostaCorreta){
-          htmlResultado = "<h1 class='resultado-final'>Resposta errada<h1>";
-      } else {
-        htmlResultado = "<h1 class='resultado-final'>Selecione seu atributo<h1>";
-      }
+    if(valorRespostaSelecionada == perguntaSorteada.respostaCorreta){
+        htmlResultado = "<h1 class='resultado-final'>Você Acertou</h1>"; 
+        ///document.getElementsByTagNameNS("span").style.backgroundColor="blue";
+
+        acertos += 1;
+        }else if(valorRespostaSelecionada < perguntaSorteada.respostaCorreta || valorRespostaSelecionada > perguntaSorteada.respostaCorreta){
+            htmlResultado = "<h1 class='resultado-final'>Resposta errada<h1>";
+            erros += 1;
+        } else {
+          htmlResultado = "<h1 class='resultado-final'>Selecione seu atributo<h1>";
+        }
+
+          document.getElementById("btnJogar").disabled = true;
+          document.getElementById("btnSortear").disabled = false;
+      
+          
+          divResultado.innerHTML = htmlResultado;
+          perguntas.splice(numeroperguntaSorteada,1 );
+        
+        
+      
+}
     
-        divResultado.innerHTML = htmlResultado;
-  
- }
+       
+ 
      
